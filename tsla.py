75793d83f5a16bb800025ffaf69168fc4fe88f6e
Jason <jason.y.jeong@gmail.com>
2020-08-18 09:04:34 -0400

    updated

--- tsla.py
+++ tsla.py
@@ -1,8 +1,6 @@
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import pylab as pl
 import datetime as dt
 
 ts = pd.read_csv('C:\\Users\\user\\.spyder-py3\\TSLA\\TSLA.csv')
@@ -34,6 +32,10 @@ ts['Volume'].rolling(50).var().plot()
 plt.show()
 
 
+
+
+
+
 #DATE BREAKDOWN
 
 #FIRST CONVERT THE DATE COLUMN TO A DATETIME OBJECT
@@ -46,33 +48,3 @@ ax = ts.plot.bar(x='Date', y='Volume', rot=0)
 
 #SCATTERPLOT OF ADJUSTED CLOSE BY DATE
 ax = sns.scatterplot(x="Date", y="Adj Close", data=ts)
-
-#%%
-
-#DATE, OPEN, HIGH, LOW, CLOSE, ADJ CLOSE, VOLUME
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
